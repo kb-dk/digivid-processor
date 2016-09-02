8f6da70c24ee20414ad48558a740189d30787d32
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2016-09-02 11:26:46 +0200

    Cleanup of utils and better handling of error dialogs

--- src/main/java/dk/statsbiblioteket/digivid/processor/Utils.java
+++ src/main/java/dk/statsbiblioteket/digivid/processor/Utils.java
@@ -2,9 +2,12 @@ package dk.statsbiblioteket.digivid.processor;
 
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextBoundsType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -13,30 +16,24 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 public class Utils {
 
-    static final Text helper;
-    static final double DEFAULT_WRAPPING_WIDTH;
-    static final double DEFAULT_LINE_SPACING;
-    static final String DEFAULT_TEXT;
-    static final TextBoundsType DEFAULT_BOUNDS_TYPE;
+    private static Logger log = LoggerFactory.getLogger(Utils.class);
 
-    static {
-        helper = new Text();
-        DEFAULT_WRAPPING_WIDTH = helper.getWrappingWidth();
-        DEFAULT_LINE_SPACING = helper.getLineSpacing();
-        DEFAULT_TEXT = helper.getText();
-        DEFAULT_BOUNDS_TYPE = helper.getBoundsType();
-    }
+    static final Text helper = new Text();
+    static final double DEFAULT_WRAPPING_WIDTH = helper.getWrappingWidth();
+    static final double DEFAULT_LINE_SPACING = helper.getLineSpacing();
+    static final String DEFAULT_TEXT = helper.getText();
 
-    public static double computeTextWidth(Font font, String text, double help0) {
+    public static double computeTextWidth(Font font, String text, double minimumWidth) {
         helper.setText(text);
         helper.setFont(font);
 
         helper.setWrappingWidth(0.0D);
         helper.setLineSpacing(0.0D);
-        double d = Math.min(helper.prefWidth(-1.0D), help0);
+        double d = Math.min(helper.prefWidth(-1.0D), minimumWidth);
         helper.setWrappingWidth((int) Math.ceil(d));
         d = Math.ceil(helper.getLayoutBounds().getWidth());
 
@@ -46,35 +43,33 @@ public class Utils {
         return d;
     }
 
-    static public void showErrorDialog(String description, Thread t, Throwable e) {
-        if (e == null) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error");
-            alert.setContentText(description);
-            alert.show();
-        } else {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error");
-            alert.setContentText(description +
-                    "An uncaught exception was thrown in thread " + t + ".\n" +
-                    "Click below to view the stacktrace, or close this " +
-                    "dialog to terminate the application.");
-            alert.show();
-        }
-        e.printStackTrace();
-        Platform.exit();
-        System.exit(-1);
+    private static void rethrow(String description, Throwable e) {
+        log.error("Fatal exception, closing down: '{}'", description, e);
+        throw new RuntimeException(e);
+
+//        Platform.exit();
+//        System.exit(-1);
+
     }
 
-    static public void showErrorDialog(Thread t, Throwable e) {
-        Utils.showErrorDialog("", t, e);
+
+    static public void errorDialog(String description, Throwable e) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setContentText(description +
+                             "Program generated a Fatal Error.'" +
+                             description + "'. Close this " +
+                             "dialog to terminate the application.");
+        Optional<ButtonType> result = alert.showAndWait();
+
+        rethrow(description,e);
     }
 
-    static public void showWarning(String informationStr) {
+    static public void warningDialog(String informationStr) {
         Alert alert = new Alert(Alert.AlertType.WARNING);
         alert.setTitle("Warning");
         alert.setContentText(informationStr);
-        alert.show();
+        alert.showAndWait();
     }
 
     public static List<List<String>> getCSV(String csvFile) throws IOException {
